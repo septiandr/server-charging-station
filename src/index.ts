0654534a0c8f6e769e63620d3a384ece86134486
tpid-septian <septian@tecnopartner.co.id>
2024-10-03 16:50:14 +0700

    change index

--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,26 @@ interface User {
   name: string
   email: string
 }
+
+// Membuat instance dari Elysia
 export const app = new Elysia()
 
-getChargingStation(app);
+// Menambahkan routes yang ada
+getChargingStation(app)
 user(app)
 car(app)
 userCar(app)
 
-// app.listen(3000)
-// console.log("-------------  Welcome  -------------")
-// console.log("=== Server Running on http://localhost:3000 ===")
+// Fungsi handler untuk Appwrite yang akan menangani request
+export default async function handler({ req, res, log, error }: any) {
+  try {
+    // Menggunakan app.handle untuk menangani request yang datang
+    const result = await app.handle(req)
+
+    // Mengirim response yang dihasilkan oleh app.handle
+    return res.json(result)
+  } catch (err) {
+    error(`Error processing request: ${err.message}`)
+    return res.json({ error: "Something went wrong" })
+  }
+}
