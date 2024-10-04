3c310f0f76abb1d1d281261ca33511f65bb013e2
tpid-septian <septian@tecnopartner.co.id>
2024-10-04 09:55:38 +0700

    serverless

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,33 @@
-import { Elysia, t } from 'elysia'
-import getChargingStation from './routes/chargingStation'
-import user from './routes/user'
-import car from './routes/car'
-import userCar from './routes/userCar'
+import { Client, Users } from "node-appwrite";
+import { dbUser } from "./config/db";
+// This Appwrite function will be executed every time your function is triggered
+export default async ({ req, res, log, error }: any) => {
+  // You can use the Appwrite SDK to interact with other services
+  // For this example, we're using the Users service
 
-// Definisi interface untuk User
-interface User {
-  id: number
-  name: string
-  email: string
-}
-export const app = new Elysia()
+  try {
+    const response = await dbUser.list();
+    res.json({
+      data:response
+    })
+    // Log messages and errors to the Appwrite Console
+    // These logs won't be seen by your end users
+    log(`Total users: ${response.total}`);
+  } catch(err) {
+    error("Could not list users: " + err.message);
+  }
 
-getChargingStation(app);
-user(app)
-car(app)
-userCar(app)
+  // The req object contains the request data
+  if (req.path === "/ping") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.text("Pong");
+  }
 
-app.listen(3000)
-console.log("-------------  Welcome  -------------")
-console.log("=== Server Running on http://localhost:3000 ===")
+  return res.json({
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
+};
