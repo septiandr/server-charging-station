196f4ef3743cf6c896abf245a01f1343c999081a
tpid-septian <septian@tecnopartner.co.id>
2024-10-04 08:32:03 +0700

    add entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,22 @@
-import { Client, Users } from "node-appwrite";
-import { convertKeys } from "./utils/convertKeys";
+import { Elysia, t } from 'elysia'
+import getChargingStation from './routes/chargingStation'
+import user from './routes/user'
+import car from './routes/car'
+import userCar from './routes/userCar'
 
-const apiKey = Bun.env.APPWRITE_API_KEY || process.env.APPWRITE_API_KEY;
-const endPoint = Bun.env.APPWRITE_END_POINT || process.env.APPWRITE_END_POINT;
-const projectId = Bun.env.APPWRITE_PROJECT_ID || process.env.APPWRITE_PROJECT_ID;
+// Definisi interface untuk User
+interface User {
+  id: number
+  name: string
+  email: string
+}
+export const app = new Elysia()
 
-// This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }: any) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-      .setEndpoint(endPoint)
-      .setProject(projectId)
-      .setKey(apiKey);
-  const users = new Users(client);
+getChargingStation(app);
+user(app)
+car(app)
+userCar(app)
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
-  }
-
-  // The req object contains the request data
-  if (req.path === "/list-charging-station") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    try {
-      const endpoint = Bun.env.OPEN_CHARGE_MAP_ENDPOINT || process.env.OPEN_CHARGE_MAP_ENDPOINT || '';
-      const apiKey = Bun.env.OPEN_CHARGE_MAP_X_API_KEY || process.env.OPEN_CHARGE_MAP_X_API_KEY || '';
-
-      const response = await fetch(endpoint, {
-          method: 'GET',
-          headers: {
-              'x-api-key': apiKey,
-              'Content-Type': 'application/json',
-          },
-      });
-
-      const data = await response.json();
-      const filteredData = convertKeys(data);
-      const mappedData = filteredData.map((item: any) => ({
-          uuid: item.uuid,
-          usageCost: item.usagecost,
-          addressInfo: item.addressinfo,
-          connections: item.connections,
-      }));
-
-      return res.json({ message: 'Success get charging station list', code: 200, data: mappedData });
-  } catch (error: any) {
-      return res.json({ message: 'Failed get charging station list', code: 400, error: error.message });
-  }
-  }
-
-  return res.json({
-    data:"Septian"
-  });
-};
+// app.listen(3000)
+// console.log("-------------  Welcome  -------------")
+// console.log("=== Server Running on http://localhost:3000 ===")
